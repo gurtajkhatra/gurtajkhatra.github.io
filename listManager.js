c4596f3553ac583979341850c2624091d6382812
gurtajkhatra <Gurtaj@Gurtajs-MacBook-Pro.local>
2015-03-07 16:59:44 -0500

    Added Javascript to send JSON Object to pebble for parsing
    Attempt #1

--- listManager.js
+++ listManager.js
@@ -1,5 +1,11 @@
+//Things to do:
+//Delete task
+//Create JSON object from the text boxes
+
+
+
 //The task number on the list
-var i = 1;
+var i = 0;
 
 
 function removeTask(loTasksDiv, taskDiv){
@@ -8,8 +14,9 @@ function removeTask(loTasksDiv, taskDiv){
 	taskList.removeChild(task);
 }
 
+//Create new textboxes for tasks to be entered
 function createTask(){
-	
+	//
 	var tasklist = document.getElementById("tasksList");
 	var newTaskElement = document.createElement("LI");
 	var taskBox = document.createElement("INPUT");
@@ -25,9 +32,9 @@ function createTask(){
 	minBox.setAttribute("id", "tMinutes");
 	secBox.setAttribute("id", "tSeconds");
 
-	taskBox.setAttribute("name", "name"+i);
-	minBox.setAttribute("name", "min"+i);
-	secBox.setAttribute("name", "sec"+i);
+	taskBox.setAttribute("name", "task");
+	minBox.setAttribute("name", "min");
+	secBox.setAttribute("name", "sec");
 
 
 	i+=1;
@@ -41,9 +48,33 @@ function createTask(){
 	return false;
 }
 
+//Converts time represented as minutes and seconds to that same value in seconds
+function convertMinutesAndSecondsToSeconds(minutes, seconds){
+	return minutes*60+seconds;
+}
+
+
+//Creates a JSON Object from the contents of the text boxes
+function createJSONFromTextBoxes(){
+	var taskNames = document.getElementsByName("task");
+	var minutes = document.getElementsByName("min");
+	var seconds = document.getElementsByName("sec");
+	
+	var jsonString = "{";
+	for (int j = 0; j<taskNames.length*2-1; j++) {
+		if(j%2 == 0) {
+			jsonString = jsonString + j + ":" + taskNames[j/2].value + ",";
+		}
+		else {
+			jsonString = jsonString + j + ":" + convertMinutesAndSecondsToSeconds(minutes[j/2].value, seconds[j/2].value) + ",";
+		}
+	}
+	jsonString = jsonString + (taskNames.length*2 -1) + ":" + convertMinutesAndSecondsToSeconds(minutes[taskNames.length-1], seconds[taskNames.length-1]) + "}";
+}
+
 function buttonPressed(){
 	document.body.style.backgroundColor = "red";
-	location.href = 'pebblejs://close#success';
+	location.href = 'pebblejs://close#success' + encodeURIComponent(JSON.stringify(createJSONFromTextBoxes()));
 }
 
 
